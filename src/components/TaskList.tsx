3b9902ff516088ee72f374f18ea7c02379b809db
Ermesson Lima <ermessonlimadossantos@hotmail.com>
2022-04-19 01:32:51 -0300

    feat: finished test

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,33 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
-  function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    function handleCreateNewTask() {
+      if(!newTaskTitle) return
+      setTasks([
+        ...tasks, {
+          id: tasks.length + 1,
+          title: newTaskTitle,
+          isComplete: false 
+        }]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => task.id === id ? { ...task, isComplete: !task.isComplete } : task))
+
+    
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    // remove task from tasks
+
+    const newTasks = tasks.filter(task => task.id !== id) 
+    setTasks(newTasks)
+
+
+
+
   }
 
   return (
